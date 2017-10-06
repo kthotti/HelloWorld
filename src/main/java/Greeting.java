733b4f7f8d6cab872ce484e2e4bc27e6c7fa8ea1
KThotti <khaleelthotti@gmail.com>
2017-10-06 15:37:53 -0700

    base version

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author kthotti
+ *
+ */
+public class Greeting {
+
+	/**
+	 * 
+	 */
+	public Greeting() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greeing");
+
+	}
+
+}
